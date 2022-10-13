65e00f3f7b23f505843c59b7b0e2f7984bffb3c7
Gustavo Herrera <gustavomherrera@gmail.com>
2022-10-13 12:46:59 -0300

    Dash
    Implementado o sistema de Dash feito pelo Luis, commit antes de testar as animação com Ghost effect para o dash

--- TesteAnimaRogue/Assets/_Scripts/Player/Player.cs
+++ TesteAnimaRogue/Assets/_Scripts/Player/Player.cs
@@ -14,7 +14,13 @@ public class Player : MonoBehaviour
     public float MoveSpeed = 5f;
     [HideInInspector] public bool facingRight = true;
     [HideInInspector] public Vector2 movement;
-    
+
+    //Variaveis Dash
+    private bool canDash = true;
+    private bool isDashing = false;
+    [SerializeField] private float dashPower = 24f;
+    [SerializeField] private float dashingCooldown = 1f;
+    private float dashingTime = 0.2f;
 
 
     private void Start()
@@ -32,11 +38,11 @@ public class Player : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         //Corrida
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             MoveSpeed = 10f;
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
+        if (Input.GetKeyUp(KeyCode.LeftControl))
         {
             MoveSpeed = 5f;
         }
@@ -55,10 +61,16 @@ public class Player : MonoBehaviour
                 combatManager.RightWeaponConfiguration();
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
 
     private void FixedUpdate()
     {
+        if (isDashing) return;
         PlayerMovement();
     }
 
@@ -82,6 +94,17 @@ public class Player : MonoBehaviour
         rb.MovePosition(rb.position + movement.normalized * (MoveSpeed * Time.fixedDeltaTime)); //faz o player se mexer
         Flip();
         
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector2(movement.x * dashPower, movement.y * dashPower);
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 
 }
